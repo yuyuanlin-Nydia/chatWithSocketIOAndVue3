336b1877e185b04f07494b3c6c1fb7a0bba7572c
YuYuan Lin <sandy6513a@yahoo.com.tw>
2023-02-17 20:57:59 +0800

    feat: change quasar css and add plugin

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -1,10 +1,15 @@
 
 import './styles/quasar.scss'
 import '@quasar/extras/material-icons/material-icons.css'
-
+import {
+  Notify,
+  Dialog
+} from 'quasar'
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
   plugins: {
+    Notify,
+    Dialog
   }
 }
