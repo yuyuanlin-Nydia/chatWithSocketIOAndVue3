9cd75821c1a9f949c264b0cd614924a261aa672f
YuYuan Lin <sandy6513a@yahoo.com.tw>
2023-03-29 11:54:24 +0800

    Vcg: Update live demo

---
+++ js/setting.a2413080.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["setting"],{"4ef5":function(n,t,e){"use strict";e.r(t);var s=e("7a23");const c={class:"about"},i=Object(s["j"])("h6",null,"This is an Setting page",-1),a=[i];function o(n,t){return Object(s["B"])(),Object(s["i"])("div",c,a)}var r=e("d959"),u=e.n(r);const b={},d=u()(b,[["render",o]]);t["default"]=d}}]);
+//# sourceMappingURL=setting.a2413080.js.map
